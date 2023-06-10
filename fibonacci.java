09e9429f444026c3565cb72f5d28f2c49dcc7895
Subham Sarangi <subhamsarangi123@gmail.com>
2023-06-10 21:31:28 +0530

    Update fibonacci.java
    code structure

--- fibonacci.java
+++ fibonacci.java
@@ -1,11 +1,14 @@
 public class fibonacci{
     static void main(){
-        System.out.print("0, 1, ");int a=0,b=1,c;
+        System.out.print("0, 1, ");
+        int a=0,b=1,c;
         for (int i=0;i<18;i++){
             c=b;b=a+b;
             a=c;
-            if (i>16)System.out.print(b); else
-            System.out.print(b+", ");
+            if (i>16)
+                System.out.print(b);
+            else
+                System.out.print(b+", ");
         }
     }
 }
